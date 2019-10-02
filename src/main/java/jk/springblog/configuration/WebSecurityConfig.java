1580ff0d49732ff620e13c8f5b3919015cb82e28
Joanna Krzyżanowska <joanna.krzyzanowska@o2.pl>
2019-10-02 16:23:57 +0200

    annotation corrected

--- src/main/java/jk/springblog/configuration/WebSecurityConfig.java
+++ src/main/java/jk/springblog/configuration/WebSecurityConfig.java
@@ -1,20 +1,19 @@
 package jk.springblog.configuration;
-
+;
 import jk.springblog.filter.SocialMediaLoginFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
-@EnableOAuth2Sso
+@EnableOAuth2Client
 @Component
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
